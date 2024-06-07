32b7a43ffb7688eade5998790af5560e6d7cceea
ShullJoseph <jshull@stanford.edu>
2024-06-07 00:51:31 -0700

    working functionality

--- myprogram.c
+++ myprogram.c
@@ -11,61 +11,55 @@
 #include "THX.h"
 
 static struct {
-    int leval;
+    int level;
     int compression_threshold;
-    int compression_ratio;
     bool backing_track;
-    int backing_track_leval;
     int length_of_recording;
     bool reverb;
 } config;
 
-uint16_t levals(uint16_t sample) {    
-    if (config.leval == -6) {
+uint16_t levels(uint16_t sample) {    
+    if (config.level == 0) {
         return 0;
     }
-
-    int adjustment = config.leval * 3000;
-
-    if ((sample + adjustment) < 0xffff) {
-        if ((sample + adjustment) > 0x0) {
-            sample += adjustment;
-        }   
-        else {
-            sample = 0x0;
-        }   
-    }   
+    if (config.level == -2) {
+        sample = (uint16_t)(sample / 2);
+    }
     else {
-        sample = 0xffff;
-    }   
-
+        sample = (uint16_t)(sample * config.level);
+    }
     return sample;
 }
 
 uint16_t compression(int sample) {
-    int threshold = config.compression_threshold * 3000;
-    if (sample > threshold) {
-        sample = threshold + ((sample - threshold) / config.compression_threshold);
+    int threshold;
+    if (config.compression_threshold == 20) {
+        threshold = 0xf000;
+    }
+    if (config.compression_threshold == 15) {
+       threshold = 0xa000;
+    }
+    if (config.compression_threshold == 10) {
+        threshold = 0x6000;
+    }
+    if (config.compression_threshold == 5) {
+        threshold = 0x1000;
+    }
+    if (config.compression_threshold == 0) {
+        threshold = 0;
+    }
+    if (sample < 0xa000) {
+        sample = threshold - (int)((sample - threshold) / 10);
     }
     return sample;
 }
 
 uint16_t backingtrack(uint16_t sample, int idx) {
-    if (config.backing_track_leval == -6) {
+    if (config.backing_track == false) {
         return sample;
     }
-    
-    int adjustment = config.backing_track_leval * 3000;
     int backing = pcm_data[idx];
-    sample += (backing + adjustment);
-
-    if (sample < 0x0) {
-        return 0x0;
-    }
-    if (sample > 0xffff) {
-        return 0xffff;
-    }
-    return ((uint16_t) sample);
+    return ((uint16_t) (sample + backing));
 }
 
 
@@ -77,13 +71,11 @@ void main ()
     mic_init();
     
     // debug------------------------
-    config.leval = 2;
-    config.compression_threshold = 2;
-    config.compression_ratio = 2;
-    config.backing_track = true;
-    config.backing_track_leval = -2;
-    config.length_of_recording = 3;
-    config.reverb = true;
+    config.level = 1;
+    config.compression_threshold = 10;
+    config.backing_track = false;
+    config.length_of_recording = 5;
+    config.reverb = false;
     // debug------------------------
 
     printf("starting mic read\n");
@@ -103,49 +95,61 @@ void main ()
             dma_disable(1);
             printf("Collection finished!\n");
             // convert to 16-bit samples
-            uint16_t *converted_samples = malloc(num_samples * sizeof(uint16_t) + 400);
-            uint16_t *reverb_buffer = malloc(num_samples * sizeof(uint16_t) + 400);
-
+            uint16_t *converted_samples = malloc(num_samples * sizeof(uint16_t));
+            uint16_t *reverb_buffer = malloc(num_samples * sizeof(uint16_t) + 4800);
+            memset(reverb_buffer, 0, (num_samples * sizeof(uint16_t) + 4800));
             for (int i = 0; i < num_samples; i++) {
                 converted_samples[i] = audio_samples[i] >> 16;
-                // process
-                converted_samples[i] = levals(converted_samples[i]);
+                
+                // make intro & outro clipping less awful
+                if (i < 44100) {
+                    converted_samples[i] = (uint16_t)(converted_samples[i] / 2);
+                }
+                if (i > (num_samples - 44100)) {
+                    converted_samples[i] = (uint16_t)(converted_samples[i] / 2);
+                }
+
+                converted_samples[i] = levels(converted_samples[i]);
                 converted_samples[i] = compression(converted_samples[i]);
+                
                 // reverb
                 if (config.reverb) {
-                    reverb_buffer[i] = converted_samples[i];
-                    reverb_buffer[i + (44 * 3)] = (int)(converted_samples[i] / 2);
-                    reverb_buffer[i + (44 * 6)] = (int)(converted_samples[i] / 4);
-                    reverb_buffer[i + (44 * 6)] = (int)(converted_samples[i] / 8);
-                } 
-
-                //backing track
-                //converted_samples[i] = backingtrack(converted_samples[i], i);
-                
-                if (converted_samples[i] < 0x0) {
-                    converted_samples[i] = 0x0;
+                    reverb_buffer[i] += converted_samples[i];
+                    reverb_buffer[i + (44 * 36)] += (int)(converted_samples[i] / 2);
+                    reverb_buffer[i + (44 * 72)] += (int)(converted_samples[i] / 4);
+                    reverb_buffer[i + (44 * 108)] += (int)(converted_samples[i] / 8);
                 }
-                if (converted_samples[i] > 0xffff) {
-                    converted_samples[i] = 0xffff;
+
+                // backing track
+                if (config.reverb) {
+                    reverb_buffer[i] = backingtrack(reverb_buffer[i], i);
+                }
+                else {
+                    converted_samples[i] = backingtrack(converted_samples[i], i);
                 }
             }
             free(audio_samples);
-            if (config.reverb) {
-                memcpy(converted_samples, reverb_buffer, (num_samples * sizeof(uint16_t) + 400));
-                free(reverb_buffer);
-            }
             i2s_init();
 
             audio_init(44100, 2, MONO);
-
+            // audio_init(44100, 2, STEREO); // want to test
             printf("1 second pause...\n");
             timer_delay_ms(1000);
             printf("starting play\n");
-            audio_write_i16_dma(converted_samples, num_samples, 0);
-            while (!dma_complete(0)) {
-                printf("playing audio\n");
+            if (config.reverb) {
+                audio_write_i16_dma(reverb_buffer, num_samples, 0);
+                while (!dma_complete(0)) {
+                    printf("playing reverb audio\n");
+                }
+                dma_disable(0);
             }
-            dma_disable(0);
+            else {
+                audio_write_i16_dma(converted_samples, num_samples, 0);
+                while (!dma_complete(0)) {
+                    printf("playing audio\n");
+                }
+                dma_disable(0);
+            }
             printf("done playing\n");
             free(converted_samples);
             return;
